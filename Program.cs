4d5dc21e9dce70a8ddc1354ac4d71be10940c174
venky <venky94.vk@gmail.com>
2023-04-23 21:39:11 +0530

    [UC1 Assignment added successfully]

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxInt maxInt = new MaxInt();
+            int intValue = maxInt.maximumValue(210, 236, 456);
+            Console.WriteLine("The Maximum Int Value is: " + intValue);
+        }
+    }
+}
